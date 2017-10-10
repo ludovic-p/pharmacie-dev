f03e462dbd213fb87ddfdd4f47fb685a5d4c3874
Tharushan <saseeskumar.tharushan@gmail.com>
2017-10-10 16:47:29 +0200

    Start Login + User entity

--- src/PharmacieDevBundle/Controller/DefaultController.php
+++ src/PharmacieDevBundle/Controller/DefaultController.php
@@ -7,6 +7,8 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use PharmacieDevBundle\Entity\User;
+
 
 class DefaultController extends Controller
 {
@@ -23,5 +25,19 @@ class DefaultController extends Controller
         $test = ["message" => "test"];
         $response = new JsonResponse();
         return $response->setContent(json_encode($test));
+    }
+
+    /**
+    * Route Get de test
+    * @Rest\Post("/login")
+    */
+    public function postLoginAction(Request $request) {
+      $data = [
+        "message" => "",
+        "data" => ""
+      ];
+      $body = json_decode($request->getContent(), true);
+      $response = new JsonResponse();
+      return $response->setContent(json_encode($body));
     }
 }
