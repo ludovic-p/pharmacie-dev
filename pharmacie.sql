46a949d8e03e017e8c246cd59bf6a46218d26dfc
Unknown <cron.peverelly@gmail.com>
2017-10-12 15:42:04 +0200

    ADD: fichier sql 2.0

--- pharmacie.sql
+++ pharmacie.sql
@@ -1,37 +1,203 @@
-CREATE DATABASE IF NOT EXISTS `pharmacie` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `pharmacie`;
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : mysql
+-- Généré le :  jeu. 12 oct. 2017 à 13:40
+-- Version du serveur :  5.7.19
+-- Version de PHP :  7.0.21
 
-CREATE TABLE IF NOT EXISTS `orders` (
-  `id_order` int(11) NOT NULL AUTO_INCREMENT,
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `pharmacie`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id_order` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
-  `status` enum('PAID','PENDING','COMPLETED','SENT','REFUNDED','CANCELED') NOT NULL,
-  `discount` int(3) NOT NULL,
-  `order_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_order`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `status` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `discount` int(11) DEFAULT NULL,
+  `order_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `order_detail` (
-  `id_order_detail` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Déchargement des données de la table `orders`
+--
+
+INSERT INTO `orders` (`id_order`, `id_user`, `status`, `discount`, `order_date`) VALUES
+(1, 1, 'PENDING', NULL, '2017-10-10 16:20:35'),
+(2, 3, 'SEND', NULL, '2017-10-05 11:17:39'),
+(3, 1, 'PENDING', NULL, '2017-10-11 20:33:47');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `order_detail`
+--
+
+CREATE TABLE `order_detail` (
+  `id_order_detail` int(11) NOT NULL,
   `id_order` int(11) NOT NULL,
   `id_product` int(11) NOT NULL,
-  `quantity` INT(5) NOT NULL,
-  PRIMARY KEY (`id_order_detail`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `products` (
-  `id_product` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) NOT NULL,
-  `description` text NOT NULL,
-  `unit_price` int(11) NOT NULL,
+--
+-- Déchargement des données de la table `order_detail`
+--
+
+INSERT INTO `order_detail` (`id_order_detail`, `id_order`, `id_product`, `quantity`) VALUES
+(1, 1, 1, 3),
+(2, 1, 2, 2),
+(3, 3, 3, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `products`
+--
+
+CREATE TABLE `products` (
+  `id_product` int(11) NOT NULL,
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `unit_price` double NOT NULL,
   `unit_stock` int(11) NOT NULL,
-  `is_parapharmacy` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id_product`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `ordonnance` tinyint(1) NOT NULL,
+  `image` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id_user` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(64) NOT NULL,
-  `password` varchar(64) NOT NULL,
-  `date_creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Déchargement des données de la table `products`
+--
+
+INSERT INTO `products` (`id_product`, `name`, `description`, `unit_price`, `unit_stock`, `ordonnance`, `image`) VALUES
+(1, 'Doliprane 500g', 'Mal de tete ? ', 3.2, 320, 0, 'test'),
+(2, 'Helicidine 300ml', 'Mal de gorge ? Ce sirop sans sucre vous conviendra !', 2.6, 261, 0, 'test'),
+(3, 'Advil 500mg', 'Pour les toux grasse', 5.84, 123, 1, 'test');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `email` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `nom` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `prenom` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `adresse` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `city` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `cp` int(11) NOT NULL,
+  `telephone` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `ordonnance` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `date_creation` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id_user`, `email`, `password`, `nom`, `prenom`, `adresse`, `city`, `cp`, `telephone`, `ordonnance`, `date_creation`) VALUES
+(1, 'test@test.fr', 'test', 'TEST', 'Test', '13, rue du test', 'Test', 12345, '1234567890', NULL, '2017-10-02 10:11:19'),
+(2, 'ludo@test.fr', 'test', 'flsfhkjsdhf', 'Ludovic', '134, rue orteaux', 'Paris', 75008, '0637465824', NULL, '2017-10-05 16:40:32'),
+(3, 'tharushan@test.fr', 'test', 'sdlfdsjlkfj', 'Tharushan', '454, avenue des olives', 'Inconnu', 93534, '0683286984', NULL, '2017-10-10 21:27:15');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id_order`),
+  ADD KEY `fk_id_user_764` (`id_user`);
+
+--
+-- Index pour la table `order_detail`
+--
+ALTER TABLE `order_detail`
+  ADD PRIMARY KEY (`id_order_detail`),
+  ADD KEY `fk_id_order_353` (`id_order`),
+  ADD KEY `fk_id_product_937` (`id_product`);
+
+--
+-- Index pour la table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id_product`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `order_detail`
+--
+ALTER TABLE `order_detail`
+  MODIFY `id_order_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `fk_id_user_764` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+
+--
+-- Contraintes pour la table `order_detail`
+--
+ALTER TABLE `order_detail`
+  ADD CONSTRAINT `fk_id_order_353` FOREIGN KEY (`id_order`) REFERENCES `orders` (`id_order`),
+  ADD CONSTRAINT `fk_id_product_937` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
