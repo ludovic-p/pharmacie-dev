057a9c48e9b45f8d50891aa806b4eece4be9cf2f
Tharushan <saseeskumar.tharushan@gmail.com>
2017-10-12 14:26:50 +0200

    Get product with image, added create Product

--- app/src/PharmacieDevBundle/Controller/ProductsController.php
+++ app/src/PharmacieDevBundle/Controller/ProductsController.php
@@ -7,7 +7,7 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use PharmacieDevBundle\Entity\User;
+use PharmacieDevBundle\Entity\Products;
 
 
 class ProductsController extends Controller
@@ -30,13 +30,43 @@ class ProductsController extends Controller
           $formatted[] = [
              'id' => $product->getIdProduct(),
              'name' => $product->getName(),
+             'image' => $product->getImage(),
              'description' => $product->getDescription(),
              'unit_price' => $product->getUnitPrice(),
              'unit_stock' => $product->getUnitStock(),
-             'isParapharmacy' => $product->isParapharmacy()
+             'ordonnance' => $product->isOrdonnance()
           ];
         }
 
         return new JsonResponse($formatted);
+    }
+
+    /**
+    * Créer un utilisateur
+    * @Rest\Post("/products")
+    */
+    public function createProductAction(Request $request){
+        $response = new JsonResponse();
+        $productPayload = json_decode($request->getContent());
+        if (!empty($productPayload->name) && !empty($productPayload->image)
+        && !empty($productPayload->description) && !empty($productPayload->unit_price)
+        && !empty($productPayload->unit_stock) && !empty($productPayload->ordonnance)){
+          $data = new Products();
+          $data->setName($productPayload->name);
+          $data->setImage($productPayload->image);
+          $data->setDescription($productPayload->description);
+          $data->setUnitPrice($productPayload->unit_price);
+          $data->setUnitStock($productPayload->unit_stock);
+          $data->setOrdonnance($productPayload->ordonnance);
+          $em = $this->getDoctrine()->getManager();
+          $em->persist($data);
+          $em->flush();
+
+          $response->setStatusCode(201);
+          return $response;
+      } else {
+          $response->setStatusCode(400);
+          return $response;
+      }
     }
 }
