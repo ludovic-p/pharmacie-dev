88b8f8e0309a3d52897ea4b497f41a55a0780074
CRON PEVERELLY Ludovic <cron.peverelly@gmail.com>
2017-10-12 02:29:05 +0200

    ADD: route HTTP delete, get et post pour les users

--- app/src/PharmacieDevBundle/Controller/UserController.php
+++ app/src/PharmacieDevBundle/Controller/UserController.php
@@ -7,7 +7,7 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use PharmacieDevBundle\Entity\User;
+use PharmacieDevBundle\Entity\Users;
 
 
 class UserController extends Controller
@@ -15,17 +15,53 @@ class UserController extends Controller
 
     /**
     * Récupere un utilisateur
-    * @Rest\Get("/user/{id}")
+    * @Rest\Get("/users/{id}")
     */
-    public function getUser($id){
-
+    public function getUserAction($id){
+        $result = $this->getDoctrine()->getRepository('PharmacieDevBundle:Users')->find($id);
+        $response = new JsonResponse();
+        $response->setContent($result->__toString());
+        return $response;
     }
 
     /**
     * Créer un utilisateur
-    * @Rest\Post("/user")
+    * @Rest\Post("/users")
     */
-    public function createUser(Request $request){
-        
+    public function createUserAction(Request $request){
+        $userPayload = json_decode($request->getContent());
+
+        $data = new Users();
+        $data->setEmail($userPayload->email);
+        $data->setMdp($userPayload->mdp);
+        $data->setNom($userPayload->nom);
+        $data->setPrenom($userPayload->prenom);
+        $data->setAdresse($userPayload->adresse);
+        $data->setCity($userPayload->city);
+        $data->setCp($userPayload->cp);
+        $data->setTelephone($userPayload->telephone);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($data);
+        $em->flush();
+
+        $response = new JsonResponse();
+        $response->setStatusCode(201);
+        return $response;
+    }
+
+    /**
+    * Supprime un utilisateur
+    * @Rest\Delete("/users/{id}")
+    */
+    public function deleteUserAction($id){
+        $data = new Users();
+        $user = $this->getDoctrine()->getRepository('PharmacieDevBundle:Users')->find($id);
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($user);
+        $manager->flush();
+        $response = new JsonResponse();
+        $response->setContent(json_encode(["message" => "Utilisateur supprimé"]));
+        return $response;
     }
 }
