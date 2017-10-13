758d2618f4d2f927dd430ed6f5aafa6bd2c5aa57
Tharushan <saseeskumar.tharushan@gmail.com>
2017-10-13 14:29:48 +0200

    Added CORS

--- app/app/AppKernel.php
+++ app/app/AppKernel.php
@@ -16,7 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            new PharmacieDevBundle\PharmacieDevBundle()
+            new PharmacieDevBundle\PharmacieDevBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
